917ba3add4f1abe010b5aea57bc5c5a931e0a4ab
Tomas Okal <tomas.okal@gmail.com>
2020-02-27 13:48:19 -0600

    Dashboard layout.

---
+++ Fantasy Football/dashboard.R
@@ -0,0 +1,55 @@
+# UI
+
+library(shinydashboard)
+library(shiny)
+
+header <- dashboardHeader(title = "Example of content")
+
+sidebar <- dashboardSidebar()
+
+body <- dashboardBody()
+
+ui <- dashboardPage(header, sidebar, body)
+
+
+ui <- dashboardPage(
+  
+  dashboardHeader(title = "Title of our content"),
+  
+  dashboardSidebar(sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    )
+    ),
+  
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "dashboard",
+              fluidRow(
+                box(plotOutput("plot1", height = 250)),
+                box(
+                  title = "Controls",
+                  sliderInput("slider", "Number of observations:", 1, 100, 50)
+                )
+              )
+      ),
+    
+    tabItem(tabName = "widgets",
+            h2("Widgets tab content")
+            )
+    
+  )
+  )
+)
+
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
+
+shinyApp(ui, server)
